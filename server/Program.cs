01eda9e39e32ff911cc2e54448463822785c1c1d
Laindell <baklangg20@gmail.com>
2026-01-01 20:29:08 +0200

    feat:Add shared gRPC project and Redis support to server
    Introduced AutoTestLab.Shared project for shared gRPC/Protobuf code and updated client/server to reference it. Removed old greet.proto and GreeterService. Added Redis caching to the server with configuration and dependencies. Updated solution and project files accordingly. Added placeholder auth.proto in shared project.

--- server/Program.cs
+++ server/Program.cs
@@ -3,12 +3,18 @@ using server.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddStackExchangeRedisCache(options => {
+    options.Configuration = builder.Configuration.GetConnectionString("RedisConnection");
+    //додаю префікс до всіх ключів у Redis
+    options.InstanceName = "AutoTestLab_";
+});
+
 builder.Services.AddGrpc();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
